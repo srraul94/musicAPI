60fb49f10e601e6e49c8097e87edc72b62686ab1
Raúl Sánchez <srraul94@gmail.com>
2019-11-28 11:37:51 +0100

    Initial Commit - Music API

---
+++ src/test/java/net/srraul94/musicAPI/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.srraul94.musicAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
